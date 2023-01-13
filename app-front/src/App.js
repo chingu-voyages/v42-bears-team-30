470dbf7cc498bbea24f62b0efbaafd03d4b00b70
Fullfifax <fullfifax1245@gmail.com>
2023-01-13 13:08:33 +0000

    [FRONT] refactor components

--- app-front/src/App.js
+++ app-front/src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
 import Homepage from './components/homepage/homepage';
-import Cards from './components/cards/cards';
-import TopCards from './components/cards/topRatedCards/topRatedCards';
 
 function App() {
   return (
     <div className="App">
       <Homepage />
-      <Cards />
-      <TopCards />
     </div>
   );
 }
