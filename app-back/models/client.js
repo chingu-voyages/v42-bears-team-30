b94ada9d4df55634159bf087e52cfdabfdaac644
pramitsingh0 <pramitsingh0@gmail.com>
2023-01-17 23:31:17 +0530

    [WIP Models] Client model created

---
+++ app-back/models/client.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const clientSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  phoneNumber: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+});
+
+module.exports = mongoose.model('Client', clientSchema);
