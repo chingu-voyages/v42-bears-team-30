ea0b5661369d6009bf317b206c7f9a8f9ec444db
pramitsingh0 <pramitsingh0@gmail.com>
2023-01-18 00:37:33 +0530

    Added fields to Reservation model

---
+++ app-back/model/reservations.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const reservationSchema = new Schema({
+  bookingDate: {
+    type: Date,
+    default: Date.now()
+  },
+  checkInDate: {
+    type: Date,
+    required: true
+  },
+  checkOutDate: {
+    type: Date
+  },
+  numGuests: {
+    type: Number,
+    default: 1,
+  },
+  clientId: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Reservation', reservationSchema);
