4a3ccf973309729590eb31359db618d44ac4dcf5
pramitsingh0 <pramitsingh0@gmail.com>
2023-01-17 21:20:00 +0530

    DB Connection function update

--- app-back/db/conn.js
+++ app-back/db/conn.js
@@ -1,26 +1,18 @@
-const { MongoClient } = require("mongodb");
+require('dotenv').config({ path: './config.env' });
+const mongoose = require('mongoose');
 const Db = process.env.ATLAS_URI;
-const client = new MongoClient(Db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
- 
-var _db;
- 
-module.exports = {
-  connectToServer: function (callback) {
-    client.connect(function (err, db) {
-      // Verify we got a good "db" object
-      if (db)
-      {
-        _db = db.db("employees");
-        console.log("Successfully connected to MongoDB."); 
-      }
-      return callback(err);
-         });
-  },
- 
-  getDb: function () {
-    return _db;
-  },
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(Db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to DB.');
+  } catch(e) {
+    console.log("Failed to connect to database!");
+    console.log(e);
+  }
 };
+
+module.exports = connectDB;
