b05d4f7f912f44f35bee01523a947fe4ba9656ed
Fullfifax <fullfifax1245@gmail.com>
2023-01-18 13:15:43 +0000

    [FRONT] add responsive to details page

--- app-front/src/components/_partials/navbar/navbar_other_pages.js
+++ app-front/src/components/_partials/navbar/navbar_other_pages.js
@@ -1,9 +1,24 @@
 import React from 'react';
+
 import './navbar_other_pages.css';
-import chinguLogo from '../../../assets/images/logo-chingu.png';
+
 import Auth from '../../auth/auth';
 
+import chinguLogo from '../../../assets/images/logo-chingu.png';
+import menu from '../../../assets/icons/menu.png';
+
 function Navbar() {
+
+    // add extra class name
+    function addExtraClassname() {
+        var extraClassName = document.getElementById("add-responsive");
+        if (extraClassName.className === "nav-right") {
+            extraClassName.className += " responsive";
+        } else {
+            extraClassName.className = "nav-right";
+        }
+    }
+
     return (
         <nav className='navbar-other-pages'>
             <div className='nav-left'>
@@ -11,7 +26,7 @@ function Navbar() {
                     <img src={ chinguLogo } alt="chingu-logo" />
                 </a>
             </div>
-            <div className='nav-right'>
+            <div id="add-responsive" className='nav-right'>
                 <ul className='nav-links'>
                     <li>
                         <a>Find a Room</a>
@@ -21,6 +36,11 @@ function Navbar() {
                     </li>
                     <li>
                         <Auth />
+                    </li>
+                    <li className='menu-hamburger'>
+                        <a onClick={ addExtraClassname }>
+                            <img src={ menu }  alt="menu" />
+                        </a>
                     </li>
                 </ul>
             </div>
