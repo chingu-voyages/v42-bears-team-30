924b5382ee60f8d65aad21c5b99e92168676c36f
Mendrika160 <rahelisonhajaina@gmail.com>
2023-01-20 12:07:12 +0100

    [FRONT] add yup and formik validation on login and register form

--- app-front/src/components/auth/Login.jsx
+++ app-front/src/components/auth/Login.jsx
@@ -1,36 +1,72 @@
 import React,{useState} from 'react'
-
+import { Formik } from "formik";
+import * as Yup from "yup";
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
 
-    const handleSubmit = event => {
-        event.preventDefault();
-        // Perform authentication here using the email and password stored in state
-    };
-
+    const schema = Yup.object().shape({
+        email: Yup.string()
+            .required("Email is a required field")
+            .email("Invalid email format"),
+        password: Yup.string()
+            .required("Password is a required field")
+            .min(8, "Password must be at least 8 characters"),
+    });
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                    <input
-                    className='form-control'
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={event => setEmail(event.target.value)}
-                    autoComplete='off'
-                    />
-                    <input
-                    className='form-control'
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={event => setPassword(event.target.value)}
-                    autoComplete='off'
-                    />
+            <Formik 
+                validationSchema={schema}
+                initialValues={{ email: "", password: "" }}
+                onSubmit={(values) => { 
+                    alert(JSON.stringify(values))
+                }}
+                
+            >{(
+                {
+                    values,
+                    errors,
+                    touched,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit,
+    
+                }
+            ) => (
+                <form onSubmit={handleSubmit}>
+                    <div className="input-from">
+                        <input
+                            className='form-control'
+                            type="email"
+                            name='email'
+                            placeholder="Email"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.email}
+                            autoComplete='off'
+                            />
+                            <span className="error-info-input">{errors.email && touched.email && errors.email}</span>
+                    </div>
+
+                    <div className="input-from">
+                        <input
+                            className='form-control'
+                            type="password"
+                            name='password'
+                            placeholder="Password"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.password}
+                            autoComplete='off'
+                            />
+                        <span className="error-info-input">{errors.password && touched.password && errors.password}</span>
+                    </div>
+              
                     <button className='btn-login' type="submit">Log In</button>
                     
-                </form>
+            </form>
+            )}
+
+            </Formik>
+            
         </>
     )
 }
--- app-front/src/components/auth/Register.jsx
+++ app-front/src/components/auth/Register.jsx
@@ -1,48 +1,104 @@
 import React,{useState} from 'react'
+import { Formik } from "formik";
+import * as Yup from "yup";
+const Login = () => {
 
-const Register = () => {
-    
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const schema = Yup.object().shape({
+        email: Yup.string()
+            .required("Email is a required field")
+            .email("Invalid email format"),
+        password: Yup.string()
+            .required("Password is a required field")
+            .min(8, "Password must be at least 8 characters"),
+        confirmPassword: Yup.string()
+            .required("Password is a required field")
+            .oneOf([Yup.ref('password'), null], 'Passwords must match')
+       
 
-    
-    const handleSubmit = event => {
-        event.preventDefault();
-        // Perform authentication here using the email and password stored in state
-        
-    };
+    });
+
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                        <input
+            <Formik 
+                validationSchema={schema}
+                initialValues={{ email: "", password: "" ,confirmPassword: ""}}
+                onSubmit={(values) => { 
+                    alert(JSON.stringify(values))
+                }}
+                
+            >{(
+                {
+                    values,
+                    errors,
+                    touched,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit,
+    
+                }
+            ) => (
+                <form onSubmit={handleSubmit}>
+
+                <div className="input-from">
+                    <input
                         className='form-control'
                         type="email"
+                        name='email'
                         placeholder="Email"
-                        value={email}
-                        onChange={event => setEmail(event.target.value)}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.email}
                         autoComplete='off'
-                        />
+                    />
+                    
+                    <span className="error-info-input">
+                        {errors.email && touched.email && errors.email}
+                    </span>
+
+                </div>
+                    
+                    <div className="input-from">
+
                         <input
-                        className='form-control'
-                        type="password"
-                        placeholder="Password"
-                        value={password}
-                        onChange={event => setPassword(event.target.value)}
-                        autoComplete='off'
+                            className='form-control'
+                            type="password"
+                            name='password'
+                            placeholder="Password"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.password}
+                            autoComplete='off'
                         />
+                        <span className="error-info-input">
+                            {errors.password && touched.password && errors.password}
+                        </span>
+                    </div>
+
+                    <div className="input-from">
                         <input
-                        className='form-control'
-                        type="password"
-                        placeholder="Confirm Password"
-                        value={password}
-                        onChange={event => setPassword(event.target.value)}
-                        autoComplete='off'
+                            className='form-control'
+                            type="password"
+                            name='confirmPassword'
+                            placeholder="Confirm Password"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.confirmPassword}
+                            autoComplete='off'
                         />
-                        <button className='btn-login' type="submit">Register</button>
-                        
-                    </form>
+                        <span className="error-info-input">{errors.confirmPassword && touched.confirmPassword && errors.confirmPassword}</span>
+                    
+                    </div>
+                    
+
+                    <button className='btn-login' type="submit">Register</button>
+                    
+            </form>
+            )}
+
+            </Formik>
+            
         </>
     )
 }
 
-export default Register
+export default Login
