58af48343f2a625bf194f83377098e83c16eb64a
pramitsingh0 <pramitsingh0@gmail.com>
2023-01-21 18:14:06 +0530

    [BACK] auth protected login and register routes

--- app-back/middleware/auth.js
+++ app-back/middleware/auth.js
@@ -1,19 +1,43 @@
-module.exports = {
-    isLoggedIn:  (req,res,next) => {
-        console.log("is logged in",req.user)
-        if(req.isAuthenticated()){
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
+const User = require('../model/ClientUser');
+const protection = asyncHandler(async (req, res, next) => {
+  let token;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    try {
+      token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_KEY);
 
-            return next()
-        }
-        req.flash('error_message', 'Please log in to access the requested page');
-        res.redirect('/auth/login')
+      req.user = await User.findById(decoded.id).select('-password');
 
-        
-    },
-    isLoggedOut : (req,res,next) => {
-        if(!req.isAuthenticated()){
-            return next()
-        }
-        res.redirect('/room')
+      return next();
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error('Not authorized');
     }
+  }
+  if (!token) {
+    res.status(401);
+    throw new Error('Not authorized, no token');
+  }
+});
+module.exports = {
+  isLoggedIn: (req, res, next) => {
+    console.log("is logged in", req.user)
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    req.flash('error_message', 'Please log in to access the requested page');
+    res.redirect('/auth/login');
+
+
+  },
+  isLoggedOut: (req, res, next) => {
+    if (!req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/room')
+  },
+  protection,
 }
