a457a667465b6f528c6abcdf2f87f33320d078ae
pramitsingh0 <pramitsingh0@gmail.com>
2023-01-21 18:14:06 +0530

    [BACK] auth protected login and register routes

--- app-back/routes/apiRoute.js
+++ app-back/routes/apiRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const {loginUserClient,registerUserClient} = require('../controller/AuthClientController');
+const { protection } = require('../middleware/auth');
 
 const router = express.Router();
 
 //route localhost:
 
-router.post('/login/client',loginUserClient);
-router.post('/register/client',registerUserClient)
+router.post('/login/client', protection, loginUserClient);
+router.post('/register/client', protection, registerUserClient)
 
 module.exports = router;
