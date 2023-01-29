7a1639db7fdb6d00bc4db970e8a75cb63b91d134
Fullfifax <fullfifax1245@gmail.com>
2023-01-29 12:17:36 +0000

    FRONT] fixed image displayed for each card

--- app-front/src/components/cards/cards.js
+++ app-front/src/components/cards/cards.js
@@ -44,55 +44,51 @@ function Cards() {
                 ? <div>Rien</div>
                 : <div className="cards-rooms-available">
                     {rooms.map(room => (
-                            room.avalaible === true &&
-                            <div key={room._id}>
-                                        <div className='card-room' key={room._id}>
-                            <div className='card-room-container'>
-                                <div className='card-room-top' onClick={(card) =>{getOneRoom(room._id)}}>
-                                    {rooms === null ? <div>No picture</div>: 
-                                        rooms.map(room => (
-                                            <img className='room-picture' src={`${host}/${room.img}`} alt="room-img"/>
-                                        ))
-                                    }
-                                    <div className='love-icon'>
-                                        <LoveButton />
-                                    </div>
-                                    <div className='room-price'>
-                                        <div className='price'>
-                                            <h4>$1000 - 5000 USD</h4>
+                        //room.avalaible === true &&
+                        <div key={room._id}>
+                            <div className='card-room' key={room._id}>
+                                <div className='card-room-container'>
+                                    <div className='card-room-top' onClick={(card) => {getOneRoom(room._id)}}>
+                                        <img key={room._id} className='room-picture' src={`${host}${room.img[0]}`} alt="room-img"/>
+                                        <div className='love-icon'>
+                                            <LoveButton />
                                         </div>
-                                        <div className='price-style'>
-                                            <p>....</p>
+                                        <div className='room-price'>
+                                            <div className='price'>
+                                                <h4>$1000 - 5000 USD</h4>
+                                            </div>
+                                            <div className='price-style'>
+                                                <p>....</p>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div className='card-room-bottom'>
-                                    <div className='room-title'>
-                                        <h4 className='room-name'>
-                                            Beach House Apartment
-                                        </h4>
-                                        <p>Comfort room</p>
-                                    </div>
-                                    <div className='room-pieces'>
-                                        <div className='pieces'>
-                                            <div className='bed-piece'>
-                                                <img src={ bedIcon } alt="bed" /> <span className='number'>2</span>
-                                            </div>
-                                            <div className='bath-piece'>
-                                                <img src={ bathIcon } alt="bath" /> <span className='number'>3</span>
-                                            </div>
-                                            <div className='car-piece'>
-                                                <img src={ carIcon } alt="car" /> <span className='number'>1</span>
-                                            </div>
-                                            <div className='pet-piece'>
-                                                <img src={ petIcon }  alt="pet" /> <span className='number'>0</span>
+                                    <div className='card-room-bottom'>
+                                        <div className='room-title'>
+                                            <h4 className='room-name'>
+                                                Beach House Apartment
+                                            </h4>
+                                            <p>Comfort room</p>
+                                        </div>
+                                        <div className='room-pieces'>
+                                            <div className='pieces'>
+                                                <div className='bed-piece'>
+                                                    <img src={ bedIcon } alt="bed" /> <span className='number'>2</span>
+                                                </div>
+                                                <div className='bath-piece'>
+                                                    <img src={ bathIcon } alt="bath" /> <span className='number'>3</span>
+                                                </div>
+                                                <div className='car-piece'>
+                                                    <img src={ carIcon } alt="car" /> <span className='number'>1</span>
+                                                </div>
+                                                <div className='pet-piece'>
+                                                    <img src={ petIcon }  alt="pet" /> <span className='number'>0</span>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                            </div>
                     ))}
                 </div>
             }
