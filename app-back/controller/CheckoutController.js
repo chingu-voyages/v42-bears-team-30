c5e78927b93a59745d8ded8965498b4ee572fac9
pramitsingh0 <pramitsingh0@gmail.com>
2023-01-31 20:13:11 +0530

    Added booking successful alert

--- app-back/controller/CheckoutController.js
+++ app-back/controller/CheckoutController.js
@@ -28,7 +28,9 @@ const createCheckoutSession = async (req, res) => {
       success_url: 'http://localhost:5000/', // Change this to the appropriate routes, for frontend team
       cancel_url: 'http://localhost:5000/error'
     });
-    res.redirect(303, session.url);
+    // res.redirect(303, session.url);
+    alert('Booking successful');
+    res.status(200)
   } catch(e) {
     console.log("error in payment");
     res.status(500).json({ error: e.message })
