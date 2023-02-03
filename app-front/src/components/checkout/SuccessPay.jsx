aa4d82da83123c3533b9a580dfaabe03be085b4c
Fullfifax <fullfifax1245@gmail.com>
2023-02-03 18:24:39 +0000

    [FRONT] stylize success payment page

--- app-front/src/components/checkout/SuccessPay.jsx
+++ app-front/src/components/checkout/SuccessPay.jsx
@@ -1,8 +1,16 @@
+import { Link } from 'react-router-dom';
+import './SuccessPay.css';
+
 const SuccessPay =  () => {
     return (
-        <>
-        <h2>Success</h2>
-        </>
+        <div className="success-container">
+            <h2 className='title-success'>
+                Booking room successfully &#128512; <br />
+            </h2>
+            <Link className="back-link" to="/">
+                Back to homepage
+            </Link>
+        </div>
     )
 }
 
