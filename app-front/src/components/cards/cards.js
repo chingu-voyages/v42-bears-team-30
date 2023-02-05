9cbee71a2d72744deb21831d81e513d84d8e3730
Fullfifax <fullfifax1245@gmail.com>
2023-02-05 12:19:46 +0000

    [FRONT] modify alt value image

--- app-front/src/components/cards/cards.js
+++ app-front/src/components/cards/cards.js
@@ -35,7 +35,7 @@ function Cards() {
                 <h2>Our room availables</h2>
             </div>
             {rooms === null
-                ? <div className='text-verify'>Check your internet connection !</div>
+                ? <div className='text-verify'>Please wait ...</div>
                 : <div className="cards-rooms-available">
                     {rooms.map(room => (
                         //room.avalaible === true &&
